5f89049b5eb4c538da784ddc1568fe632a2d1536
Corey Kelley <coreyboy1820@hotmail.com>
2022-07-10 17:45:54 -0700

    finished front end for log-in

--- login.go
+++ login.go
@@ -1,13 +1,19 @@
 package main 
 
 import(
+	"log"
 	_ "database/sql"
-	_ "html/template"
+	"html/template"
 	"net/http"
 	_ "Personal-Website/Home-Website/user"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	
+	t, err := template.ParseFiles("logIn.html")
+	if err != nil {
+		log.Println(err)
+	}
+	tm := make(map[string]interface{})
+	t.Execute(w, tm)
 }
--- main.go
+++ main.go
@@ -13,11 +13,10 @@ import(
 var db *sql.DB
 
 func main(){
-
+	http.HandleFunc("/logIn/", LoginHandler)
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/aboutMe/", AboutMeHandler)
 	http.HandleFunc("/comments/", CommentsHandler)
 	log.Println("Starting server on localhost:8080")
 	http.ListenAndServe(":8080", nil)
-
 }
